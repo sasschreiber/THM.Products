0904e87d01e905e3e8852eaa49c7aa2783c25d9b
Saskia Schreiber <sas.schreiber@gmail.com>
2015-04-06 12:50:12 +0200

    [TASK] Reduce amount of created products to ensure faster dev testing

--- Scripts/benchmark.sh
+++ Scripts/benchmark.sh
@@ -23,7 +23,7 @@ echo "Clearing database..."
 # First test: Flat hierarchy - no subproducts or properties
 echo "Running test #1 (Flat)..."
 printf "\n\n########### Flat Hierarchy Test ###########\n\n" >> $LOGFILE
-./flow benchmark:write --productsCount 5000 --propertiesPerProduct 0 --childrenDepth 0 --childrenLength 0 >> $LOGFILE
+./flow benchmark:write --productsCount 500 --propertiesPerProduct 0 --childrenDepth 0 --childrenLength 0 >> $LOGFILE
 printf "\n\n" >> $LOGFILE
 
 #Run findAll
@@ -41,7 +41,7 @@ echo "Clearing database..."
 # Second test: Many subproducts (references) but no properties
 echo "Running test #2 (Referencing)..."
 printf "\n\n########### Referencing Test ###########\n\n" >> $LOGFILE
-./flow benchmark:write --productsCount 1000 --propertiesPerProduct 0 --childrenDepth 4 --childrenLength 1 >> $LOGFILE
+./flow benchmark:write --productsCount 100 --propertiesPerProduct 0 --childrenDepth 4 --childrenLength 1 >> $LOGFILE
 printf "\n\n" >> $LOGFILE
 
 #Run findAll
