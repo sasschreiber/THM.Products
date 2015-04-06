afb1e440515dd8857a3d85b16a557854babe36fd
Saskia Schreiber <sas.schreiber@gmail.com>
2015-04-06 12:59:10 +0200

    [FEATURE] Add two new tests to bash script: Property and Combination

--- Scripts/benchmark.sh
+++ Scripts/benchmark.sh
@@ -23,7 +23,7 @@ echo "Clearing database..."
 # First test: Flat hierarchy - no subproducts or properties
 echo "Running test #1 (Flat)..."
 printf "\n\n########### Flat Hierarchy Test ###########\n\n" >> $LOGFILE
-./flow benchmark:write --productsCount 500 --propertiesPerProduct 0 --childrenDepth 0 --childrenLength 0 >> $LOGFILE
+./flow benchmark:write --productsCount 500 --propertiesPerProduct 0 --childrenDepth 0 --childrenLength 0 --productsPerFlush 30 >> $LOGFILE
 printf "\n\n" >> $LOGFILE
 
 #Run findAll
@@ -41,7 +41,39 @@ echo "Clearing database..."
 # Second test: Many subproducts (references) but no properties
 echo "Running test #2 (Referencing)..."
 printf "\n\n########### Referencing Test ###########\n\n" >> $LOGFILE
-./flow benchmark:write --productsCount 100 --propertiesPerProduct 0 --childrenDepth 4 --childrenLength 1 >> $LOGFILE
+./flow benchmark:write --productsCount 100 --propertiesPerProduct 0 --childrenDepth 4 --childrenLength 1 --productsPerFlush 30 >> $LOGFILE
+printf "\n\n" >> $LOGFILE
+
+#Run findAll
+# (Add more tests here)
+./flow benchmark:findAll >> $LOGFILE
+
+#Cleanup
+echo "Clearing database..."
+./flow benchmark:cleandb
+./flow flow:cache:flush --force > /dev/null
+
+######## 
+# Third test: Many properties, embedded in the couch case
+echo "Running test #3 (Embedded properties)..."
+printf "\n\n########### Property Test ###########\n\n" >> $LOGFILE
+./flow benchmark:write --productsCount 500 --propertiesPerProduct 5 --childrenDepth 0 --childrenLength 0 --productsPerFlush 30 >> $LOGFILE
+printf "\n\n" >> $LOGFILE
+
+#Run findAll
+# (Add more tests here)
+./flow benchmark:findAll >> $LOGFILE
+
+#Cleanup
+echo "Clearing database..."
+./flow benchmark:cleandb
+./flow flow:cache:flush --force > /dev/null
+
+######## 
+# Fourth test: Combined case
+echo "Running test #4 (Combination)..."
+printf "\n\n########### Combination Test ###########\n\n" >> $LOGFILE
+./flow benchmark:write --productsCount 500 --propertiesPerProduct 5 --childrenDepth 4 --childrenLength 1 --productsPerFlush 30 >> $LOGFILE
 printf "\n\n" >> $LOGFILE
 
 #Run findAll
@@ -54,3 +86,5 @@ echo "Clearing database..."
 ./flow flow:cache:flush --force > /dev/null
 
 echo "All tests done."
+
+
